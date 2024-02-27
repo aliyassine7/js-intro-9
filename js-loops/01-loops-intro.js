f6b82b29ea9858993c1fef3413c7a58481aae31e
aliyassine7 <aliyassine43@gmail.com>
2024-02-26 20:39:50 -0600

    added loops

--- js-loops/01-loops-intro.js
+++ js-loops/01-loops-intro.js
@@ -1,10 +1,25 @@
 // print 1, 2, 3, 4, 5, 6,...
+/*
+for loop - i loop - traditional loop
+
+for(initialization; termination_condition; update)
+*/
 
 
-
-for(let num = 1; num <= 10; num++) { // num = 1
-    console.log(num);
+for(let i = 1; i <= 10; i++) { // num = 1
+    console.log(i);
 }
 
 
 console.log('End of the program');
+
+// 0, 2, 4, 6, 8, 10
+for(let i = 0; i <= 10; i+=2) {
+    console.log(i);
+}
+
+
+// 50, 40, 30, 20, 10, 0
+for(let i = 50; i >= 0; i-=10) {
+    console.log(i)
+}
