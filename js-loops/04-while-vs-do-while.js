6a405f19405b1c4056689fae582af9db4b26f33c
aliyassine7 <aliyassine43@gmail.com>
2024-03-02 14:41:26 -0600

    added new lines

--- js-loops/04-while-vs-do-while.js
+++ js-loops/04-while-vs-do-while.js
@@ -1,31 +1,17 @@
 const {generateRandomNumber} = require('../utils/MathHelper.js');
+
 let i = 1, j = 1;
 
-while(i <= 3) { 
-    console.log(`This is while loop block`);
+while(i <= 3) { // false
+    console.log('This is while loop block');
     i++;
 }
 
 
 do {
-    console.log(`This is do-while loop block`);
+    console.log('This is do-while loop block');
     j++;
 } while(j <= 3);
-
-
-// Phone pin
-const setPassword = 1234;
-let generatedCode;
-let attempt = 0;
-
-do {
-    // enter passcode
-    generatedCode = generateRandomNumber(1000, 9999);
-    attempt++;
-} while(setPassword !== generatedCode);
-
-console.log(`We found the pin after ${attempt} attempts!`);
-
 
 
 // do {
@@ -49,9 +35,6 @@ console.log(`We found the pin after ${attempt} attempts!`);
 //     }
 
 // } while(setPassword !== generatedCode);
-
-// console.log(`We found the pin after ${attempt} times!`);
-
 
 const gameNumber = 4;
 let guessedNumber;
@@ -79,44 +62,40 @@ if(found) {
 }
 
 
-
-// Generate an even number bt 1 and 10
+// Generate an even number bt 1 and 10 
 // Calculate how many attempts it takes to generate it
 
 // 1. do-while
-let att = 0;
-let rNum; // undefined
 
-do {
+let att = 0;
+let rNum; // undefined 
+
+do{
     rNum = generateRandomNumber(1, 10);
     att++;
 } while(rNum % 2 !== 0);
- 
-console.log(`We found an even number after ${att} time/s which is ${rNum}`);
+
+console.log(`We found an even number after ${att} time/s which is ${rNum}`); 
+
 
 // 2. while 
-let r1 = generateRandomNumber(1, 10);
-let count = 0;
-i = 0;
 
-while(i <= r1) {
-    count++;
-    i += 2;
-}
+let att1 = 0;
+let rNum1; // undefined 
 
-console.log(count)
-
-
-// 2. while
 while(true) {
     rNum1 = generateRandomNumber(1, 10);
     att1++;
+
+    if(rNum1 % 2 === 0) break;
 }
 
+console.log(`We found an even number after ${att1} time/s which is ${rNum1}`); 
 
-// for
+
+// 3. for loop
 let att2 = 0;
-let rNum2; // undefined
+let rNum2; // undefined 
 
 for( ; ; ) {
     rNum2 = generateRandomNumber(1, 10);
@@ -125,4 +104,4 @@ for( ; ; ) {
     if(rNum2 % 2 === 0) break;
 }
 
-console.log(`We found an even number after ${att2} time/s which is ${rNum2}`);
+console.log(`We found an even number after ${att2} time/s which is ${rNum2}`); 
