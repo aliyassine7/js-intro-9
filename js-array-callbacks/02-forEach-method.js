4ccc06a538cc6af260c02f807ee41aef3aabc678
aliyassine7 <aliyassine43@gmail.com>
2024-03-09 13:53:55 -0600

    added methods

--- js-array-callbacks/02-forEach-method.js
+++ js-array-callbacks/02-forEach-method.js
@@ -19,4 +19,44 @@ names.forEach(name => name.toLowerCase().startsWith('j') ? console.log(name) : n
 
 names.forEach(name => {
     if(name.toLowerCase().startsWith('j')) console.log(name);
-})
+});
+
+
+// Count how many names have 4 letters -> 3
+// This task is better to solve with reduce method
+let count4LetterName = 0;
+
+names.forEach(name => {
+    if(name.length === 4) count4LetterName++;
+});
+
+console.log(count4LetterName);
+
+
+// Print each number
+const numbers = [5, 10, 3, 0, -2];
+
+numbers.forEach(number => console.log(number));
+
+
+// Print true for even numbers and false for odd numbers
+numbers.forEach(number => console.log(number % 2 === 0));
+
+
+// Create a new array that multiplies and stores each of the numbers element by 3 -> [ 15, 30, 9, 0, -6 ]
+// We can solve this with map() as well
+const multiplyBy3 = [];
+
+numbers.forEach(number => multiplyBy3.push(number * 3));
+
+console.log(multiplyBy3);
+
+
+// Create a new array that stores even number -> [10, 0, -2]
+// We can also use filter() to solve
+const newArr = [];
+
+numbers.forEach(number => number % 2 === 0 ? newArr.push(number) : null);
+
+console.log(newArr);
+
---
+++ js-array-callbacks/03-map-filter-reduce.js
@@ -0,0 +1,10 @@
+/*
+Assume you are given an array
+
+const arr = [el1, el2, el3, el4, el5, el6];
+
+
+Count, find sum, average, min, max, shortest string, longest string: simply result being one simple result -> reduce method
+returning some of the elements (subarray of the original) -> filter method
+returning a same size new array with modified original array elements -> map method
+*/
