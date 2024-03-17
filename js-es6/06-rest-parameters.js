5085497e86296f8b6e13946c97eaaa73858c125a
aliyassine7 <aliyassine43@gmail.com>
2024-03-17 10:35:50 -0500

    added rest parameters

--- js-es6/06-rest-parameters.js
+++ js-es6/06-rest-parameters.js
@@ -12,3 +12,19 @@ sum(1); // 1
 sum(1, 2); // 3
 sum(1, 2, 3); // 6
 sum(1, 2, 3, 4); // 10
+
+
+
+const product = (...args) => {
+    if(args.length === 0) return 0; // if there are no arguemnts then return 0
+
+    let result = args.reduce((acc, curr) => acc * curr, 1)
+
+    return result;
+}
+
+console.log(product()); // 0
+console.log(product(0)); // 0
+console.log(product(5)); // 5
+console.log(product(2, 5)); // 10
+console.log(product(2, 5, 3)); // 30
