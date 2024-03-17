b42276881389e565a334b84c6a62ea50afcf4d8b
aliyassine7 <aliyassine43@gmail.com>
2024-03-17 10:26:27 -0500

    added rest parameters

--- js-es6/06-rest-parameters.js
+++ js-es6/06-rest-parameters.js
@@ -1,9 +1,14 @@
-function sum(n1, n2) {
-    console.log(n1 + n2);
+// Rest parameters allow us to create a method with any number of arguments 
+
+function sum(...args) { // any number of arguments as an array
+    let sum = args.reduce((acc, curr) => acc + curr, 0);
+
+    console.log(sum);
 }
 
 // Method overloading -> same method name but different arguments
 
-sum(1); // NaN
+sum(1); // 1
 sum(1, 2); // 3
-sum(1, 2, 3); // 3
+sum(1, 2, 3); // 6
+sum(1, 2, 3, 4); // 10
