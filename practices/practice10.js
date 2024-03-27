55b73e5f0dab4394c502e792a0ba99f705e60523
aliyassine7 <aliyassine43@gmail.com>
2024-03-27 17:04:08 -0500

    added notes

--- practices/practice10.js
+++ practices/practice10.js
@@ -51,21 +51,45 @@ const superPlayer = new SuperPlayer('Super Player', 3);
 const heroPlayer = new HeroPlayer('Hero Player', 10);
 
 // Task Part 5
-player1.attack(player2);
+/*
+Execute the attack() method for player1 over player2
+-this will decrease player2's score by 1 and increase player1's score by 1
+*/
+player1.attack(player2); 
 
-player1.attack(superPlayer);
+/*
+Execute the attack() method for player1 over superPlayer
+-this will decrease superPlayer's score by 1 and increase player1's score by 1
+*/
+player1.attack(superPlayer); 
 
+/*
+Execute the superAttack() method for superPlayer over player2, player1 and heroPlayer
+-this will decrease player2, player1 and heroPlayer's score by 2 and increase superPlayer's score by 6
+*/
 superPlayer.superAttack(player2);
 superPlayer.superAttack(player1);
 superPlayer.superAttack(heroPlayer);
 
+/*
+Execute the heal() method for heroPlayer over player1 and player2
+-this will increase player1 and player2's score by 1
+*/
 heroPlayer.heal(player1);
 heroPlayer.heal(player2);
 
+/*
+Execute the attack() method for player1, player2 and heroPlayer over superPlayer
+-this will decrease superPlayer's score by 3 and increase player1, player2 and heroPlayer's score by 1
+*/
 player1.attack(superPlayer);
 player2.attack(superPlayer);
 heroPlayer.attack(superPlayer);
 
+/*
+Execute the damage() method for player1 and player2
+-this will decrease player1 and player2's score by 1
+*/
 player1.damage();
 player2.damage();
 
