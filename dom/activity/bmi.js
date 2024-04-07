bad9818cb378854ea95a96c479755aa843369aff
aliyassine7 <aliyassine43@gmail.com>
2024-04-07 12:23:46 -0500

    added html

---
+++ dom/activity/bmi.js
@@ -0,0 +1,15 @@
+/**
+ * bmi = weight / height ^ 2
+ */
+
+// Step 1
+// Get elements
+// input, btn, result
+
+// Step 2
+// Attach an event
+// Get values
+// Do calculations
+
+// Step 3
+// Show result => append to DOM
